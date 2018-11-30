90068b95409dc2c3057826e5755f28c342697f63
TacomasterPaco <45496751+TacomasterPaco@users.noreply.github.com>
2018-11-30 16:06:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,11 +13,10 @@ void desplegarMenu(); //Esta función nos permite desplegar el menú.
 void Singleplayer(); //Cuando se presiona 1 en el menú, nos lleva a la función SinglePlayer
 void Multiplayer(); //Cuando presionamos 2 en el menú, nos lleva a la función MultiPlayer
 void color(int X); //Ésta función hace referencia a un comando que nos permite cambiar el color de las letras dependiendo de donde se selccione
-void playerturn (int& x,int& y); //Ésta función hace referencia a cuando le toca jugar a la persona,
+void playerturn (int&,int&); //Ésta función hace referencia a cuando le toca jugar a la persona,
                                     //En caso de que sea Multiplayer la función se llamará 2 veces
                                     // y si es singleplayer solo una vez.
-void computerturn(int&x,int&y);//Ésta función se llama cuando se selecciona singleplayer.
-
+void computerturn(int&,int&,int&,int,int);//Ésta función se llama cuando se selecciona singleplayer.
 int main()
 {
    int op=0; //Aquí se inicializa Op, que es la operación que seleccionará el usuario en el menú
@@ -77,9 +76,8 @@ void Singleplayer(void)
 void Multiplayer(void)
 {
     cout<<"Se seleccionó el modo multiplayer"<<endl;
-    {
     const int martizss=6;
-    int first_to_go=1 ,turns = 0, boats_sunk1 =8,boats_sunk2 =8,x=0,y=0,
+    int first_to_go=1 ,turns = 0, boats_sunk1 =8,boats_sunk2 =8,x=0,y=0,lasthitcomx=0,lasthitcomy=0,lasthitcompturn=0,
     matrizplyer2[martizss][martizss]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     matrizplyer1[martizss][martizss]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//0=water,1=miss,2=boat,3=hit
     /*srand(time(0));
@@ -90,18 +88,18 @@ void Multiplayer(void)
             if((matrizplyer2[x][y]%2)==0){//if water or boat increase value by one to chage to miss ot hit
                 matrizplyer2[x][y]++;
                 if(matrizplyer2[x][y]==3){
-                    cout<<"\nAcertaste\n";//of hit, says and reduces boats leaf to win and turn-- do it doesnt count the free hit as a turn
+                    cout<<"\nhit\n";//of hit, says and reduces boats leaf to win and turn-- do it doesnt count the free hit as a turn
                     boats_sunk1--;
                     turns--;
                     continue;
                 }
                 else{
-                    cout<<"\nFallaste\nJugador 2\n";
+                    cout<<"\nmixx\nnext2\n";
                     first_to_go++;
                 }
             }
             else if((matrizplyer2[x][y]%2)==1){
-                cout<<"\nYa disparaste ahí\n";
+                cout<<"\nalredy fired there\n";
                 turns--;
                 continue;
             }
@@ -111,59 +109,84 @@ void Multiplayer(void)
             if((matrizplyer1[x][y]%2)==0){
                 matrizplyer1[x][y]++;
                 if(matrizplyer1[x][y]==3){
-                    cout<<"\nAcertaste\n";
+                    cout<<"\nhit\n";
                     boats_sunk2--;
                     turns--;
                     continue;
                 }
                 else{
-                    cout<<"\nFallaste\nJugador 1\n";
+                    cout<<"\nmiss\nnext1\n";
                     first_to_go--;
                 }
             }
             else if((matrizplyer1[x][y]%2)==1){
-                cout<<"\nYa disparaste ahi\n";
+                cout<<"\naleady fired there\n";
                 turns--;
                 continue;
             }
         }
     }
 }
-}
+
 void color(int X)
 {
 SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X);
 }
-void computerturn(int&x,int&y)
+void computerturn(int&x,int&y,int&lasthitcompturn,int lasthitcomx, int lasthitcomy)
 {
-    cout<<"si";
+    cout<<"si";//cumputer rurjr
     srand(time(0));
     x=rand() % 5+0;
     srand(time(0));
     y=rand() % 5+0;
+    if(lasthitcompturn)
+    {
+        switch (lasthitcompturn){
+            case 1:
+                x=lasthitcomx+1;
+                y=lasthitcomy;
+                lasthitcompturn++;
+                return;
+            case 2:
+                x=lasthitcomx;
+                y=lasthitcomy+1;
+                lasthitcompturn++;
+                return;
+            case 3:
+                x=lasthitcomx-1;
+                y=lasthitcomy;
+                lasthitcompturn++;
+                return;
+            case 4:
+                x=lasthitcomx;
+                y=lasthitcomy-1;
+                lasthitcompturn=0;
+                return;
+        }
+    }
     if(x>5||x<-1){
-        computerturn(x,y);
+        computerturn(x,y,lasthitcompturn,lasthitcomx,lasthitcomy);
     }
     else if(y>5||y<-1){
-        computerturn(x,y);
+        computerturn(x,y,lasthitcompturn,lasthitcomx,lasthitcomy);
     }
     else
         cout<<"\n";
     return;
 }
 void playerturn(int&x,int&y){
-    cout<<" Ingrese la coordenada en x:";
+    cout<<"x:";
     cin>>x;
-    cout<<"Ingrese la coordenana en y:";
+    cout<<"y:";
     cin>>y;
     x--;
     y--;
     if(x>5||x<-1){
-        cout<<"Ingresar una cordenada correcta\n";
+        cout<<"poner cordenada correcta\n";
         playerturn(x,y);
     }
     else if(y>5||y<-1){
-        cout<<"Ingrese una cordenada correcta\n";
+        cout<<"poner cordenada correcta\n";
         playerturn(x,y);
     }
     else
