466f7b1d7782258646fc18124a5c0dd7955be994
Daneitor07 <45240066+Daneitor07@users.noreply.github.com>
2018-11-30 14:29:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,84 @@
 #include <iostream>
 #include <iomanip>
-#include <stdlib.h>
-#include <stdio.h>
-#include <time.h>
+#include<cstdlib>
+#include <windows.h>
+#include <string>
 
 using namespace std;
 
+void desplegarMenu();
+void Singleplayer();
+void Multiplayer();
+void color(int X);
 void playerturn (int& x,int& y);
 void computerturn(int&x,int&y);
 
-
 int main()
-
 {
+   int op=0;
+
+    while(op!=3)
+    {
+        desplegarMenu();
+        cin>>op;
+        if(op>0&&op<4)
+        {
+        switch(op)
+        {
+        case 1:
+            system("cls");
+            Singleplayer();
+            break;
+        case 2:
+            system("cls");
+            Multiplayer();
+
+            break;
+        case 3:
+            system("cls");
+            cout<<"Off"<<endl;
+            break;
+        }
+        }
+    if(op<1||op>3)
+        {
+            system("cls");
+            color(4);
+            cout<<endl<<"\tINGRESE UNA OPCION CORRECTA"<<endl;
+        }
+    }
+return 0;
+}
+void desplegarMenu(void)
+{
+    color(48);
+    cout<<("\n");
+    cout<<("\t    PixelShip");
+    cout<<("\n");
+    color(15);
+    cout<<("\t\t$\t\t\t\n\t\t$|\n\t\t$|$\n\t\t$|$$\n\t\t$|$$$\n\t\t$|$$$$");
+    cout<<("\n\t\t |\n\t\t |");
+    color(14);
+    cout<<("\n\t$$$$$$$$$$$$$$$$$$$$$$$$$\n\t -----------------------");
+    cout<<("\n\t  $$$$/)$$$$$$$$$$$$$$$\n\t~~~$$$(/$$$$$$$$$$$$$$~~~\n\t~~~~$$$$$$$$$$$$$$$$$~~~~");
+    cout<<("\n\t~~~~~~~~~~~~~~~~~~~~~~~~~\n\t~~~~~~~~~~~~~~~~~~~~~~~~~");
+    color(8);
+    cout<<("\n\n\t1- Un Jugador\n\n\t2- Multrijugador\n\n\t3- Salir\n\n\tIntroduce una opción:");
+}
+void Singleplayer(void)
+{
+    cout<<"Se seleccionó el modo un jugador"<<endl;
+}
+void Multiplayer(void)
+{
+    cout<<"Se seleccionó el modo multiplayer"<<endl;
+    {
     const int martizss=6;
-    int first_to_go=1 ,turns = 0, boats_sunk1 =8,boats_sunk2 =8,x=0,y=0,lasthitx=0,lastjity=0,lasthitmarker=0,
+    int first_to_go=1 ,turns = 0, boats_sunk1 =8,boats_sunk2 =8,x=0,y=0,
     matrizplyer2[martizss][martizss]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     matrizplyer1[martizss][martizss]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};//0=water,1=miss,2=boat,3=hit
     /*srand(time(0));
-    first_to_go=rand() % 2+1;*///randi for who goes first
+    first_to_go=rand() % 2+1;*/
     for(turns;turns<72;turns++){//limit 72 due to number of possilbe turnsorhits
         if(first_to_go==1){
             playerturn(x,y);//func to set coord og shoot
@@ -64,6 +124,28 @@ int main()
         }
     }
 }
+}
+void color(int X)
+{
+SetConsoleTextAttribute(GetStdHandle (STD_OUTPUT_HANDLE),X);
+}
+void computerturn(int&x,int&y)
+{
+    cout<<"si";
+    srand(time(0));
+    x=rand() % 5+0;
+    srand(time(0));
+    y=rand() % 5+0;
+    if(x>5||x<-1){
+        computerturn(x,y);
+    }
+    else if(y>5||y<-1){
+        computerturn(x,y);
+    }
+    else
+        cout<<"\n";
+    return;
+}
 void playerturn(int&x,int&y){
     cout<<"x:";
     cin>>x;
@@ -78,23 +160,6 @@ void playerturn(int&x,int&y){
     else if(y>5||y<-1){
         cout<<"poner cordenada correcta\n";
         playerturn(x,y);
-    }
-    else
-        cout<<"\n";
-    return;
-}
-void computerturn(int&x,int&y){
-    cout<<"si";
-
-    srand(time(0));
-    x=rand() % 5+0;
-    srand(time(0));
-    y=rand() % 5+0;
-    if(x>5||x<-1){
-        computerturn(x,y);
-    }
-    else if(y>5||y<-1){
-        computerturn(x,y);
     }
     else
         cout<<"\n";
